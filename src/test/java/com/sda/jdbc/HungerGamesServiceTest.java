c91c1327dbef5bb95b23f1cba49cfd16915ef76b
Damian Konarowski <konar9108@gmail.com>
2020-06-23 22:30:06 +0200

    added junit and junitparams dependencies, added few basic junit @Tests

---
+++ src/test/java/com/sda/jdbc/HungerGamesServiceTest.java
@@ -0,0 +1,81 @@
+package com.sda.jdbc;
+
+import com.sda.entities.Judges;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HungerGamesServiceTest {
+
+
+    HungerGamesService hungerGamesService = new HungerGamesService();
+
+
+    @Test
+    void openConnection() {
+
+
+        hungerGamesService.openConnection();
+
+
+        assertTrue(hungerGamesService.getEntityManager().isOpen());
+
+    }
+
+    @Test
+    void closeConnection() {
+
+        hungerGamesService.openConnection();
+
+        hungerGamesService.closeConnection();
+
+
+       assertFalse(hungerGamesService.getEntityManager().isOpen());
+
+    }
+
+    @Test
+    void addJudge() {
+
+        hungerGamesService.openConnection();
+
+        Judges judge = new Judges();
+        judge.setFirst_name("Damian");
+        judge.setLast_name("Damianovic");
+        judge.setAge(28);
+        hungerGamesService.getEntityManager().getTransaction().begin();
+        hungerGamesService.getEntityManager().persist(judge);
+        hungerGamesService.getEntityManager().getTransaction().commit();
+
+        int id = judge.getJudge_id();
+
+        assertNotNull( hungerGamesService.getEntityManager().find(Judges.class, id));
+
+    }
+
+
+    @Test
+    void findAllJudges() {
+    }
+
+    @Test
+    void addTeam() {
+    }
+
+    @Test
+    void findAllTeams() {
+    }
+
+    @Test
+    void findTeamById() {
+    }
+
+    @Test
+    void getRandomTeams() {
+    }
+
+    @Test
+    void getRandomJudges() {
+    }
+}
